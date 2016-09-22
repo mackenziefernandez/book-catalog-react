32e90ab7160bc41a179a52c5c1db059caa248da7
mfernandez-rally <mfernandez@rallydev.com>
2016-09-21 22:42:15 -0600

    Add book form and beginning of isbn blur

--- src/app/components/admin/add_book.jsx
+++ src/app/components/admin/add_book.jsx
@@ -7,7 +7,15 @@ class AddBook extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ''
+      message: '',
+      isbn: '',
+      title: '',
+      authorFirst: '',
+      authorLast: '',
+      status: false,
+      format: 'paperback',
+      length: '',
+      imageUrl: ''
     }
     this.onFormSubmit = this.onFormSubmit.bind(this);
 
@@ -16,16 +24,41 @@ class AddBook extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     var email = this.refs.email.value;
-    this.props.resetPasswordEmail(email).then(data => {
+    this.props.addBook(email).then(data => {
 
       if (data.payload.errorCode)
         this.setState({message: data.payload.errorMessage})
       else
         this.setState({message: "Please see your email!"})
 
-
     });
 
+  }
+
+  setISBN = event => this.setState({isbn:event.target.value});
+
+  getBookInfo = () => {
+    const isbn = this.state.isbn;
+    console.log("searching the ISBN: " + isbn);
+    // var url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn;//9780765356130';
+    //
+    // $http({ method: 'GET', url }).then(function successCallback(response) {
+    //   if (response.data.totalItems == 0) {
+    //     console.log("No books came back");
+    //     // Add iframe to screen with amazon search results
+    //     current.amazonURL = "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dstripbooks&field-keywords="+isbn;
+    //   } else {
+    //     var volumeInfo = response.data.items[0].volumeInfo;
+    //     if (typeof current.title == 'undefined') {current.title = volumeInfo.title;}
+    //     console.log(volumeInfo);
+    //     if (typeof current.authorFirst == 'undefined') {current.authorFirst = _.initial(volumeInfo.authors[0].split(" ")).join(" ");}
+    //     if (typeof current.authorLast == 'undefined') {current.authorLast = _.last(volumeInfo.authors[0].split(" "));}
+    //     if (typeof current.length == 'undefined') {current.length = volumeInfo.pageCount;}
+    //     if (typeof current.imageURL == 'undefined') {current.imageURL = volumeInfo.imageLinks.thumbnail;}
+    //   }
+    // }, function errorCallback(response) {
+    //   console.log(response);
+    // });
   }
 
   render() {
@@ -33,13 +66,63 @@ class AddBook extends Component {
 
       <div className="col-md-4">
         <form role="form" onSubmit={this.onFormSubmit}>
-          <h4>{this.state.message}</h4>
+          <h4>
+            {this.state.message}
+          </h4>
           <div className="form-group">
-            <label htmlFor="txtEmail">Add Book Here!</label>
-            <input type="email" className="form-control" id="txtEmail" ref="email" placeholder="Enter email"
-                   name="email"/>
+            <div className="side-by-side">
+              <div>
+                {this.state.imageURL !== '' && <img src={this.state.imageURL} />}
+              </div>
+              <div>
+                <div>
+                  <input name='isbn' id="{{isbn}}Input" value={this.state.isbn} onChange={this.setISBN} onBlur={this.getBookInfo} placeholder="ISBN" />
+                </div>
+                <div>
+                  <input
+                    name='title'
+                    value={this.state.title}
+                    placeholder="Title" />
+                </div>
+                <div>
+                  <input
+                    value={this.state.authorFirst}
+                    placeholder="Author First" />
+                  <input
+                    value={this.state.authorLast}
+                    placeholder="Author Last" />
+                </div>
+                <div>
+                  <input
+                    value={this.state.length}
+                    placeholder="# of Pages" />
+                  <input
+                    value={this.state.imageURL}
+                    placeholder="Image URL" />
+                </div>
+                <div>
+                  <input
+                    id="status"
+                    type="checkbox"
+                    value={this.state.status} />
+                  <label htmlFor="status">finished</label>
+                  <input
+                    type="radio"
+                    ng-model="editBookCtrl.format"
+                    value="paperback" />
+                  <label>paperback</label>
+                  <input
+                    type="radio"
+                    ng-model="editBookCtrl.format"
+                    value="hardback" />
+                  <label>hardback</label>
+                </div>
+              </div>
+            </div>
           </div>
-          <button type="submit" className="btn btn-default btn-block">Save</button>
+          <button
+            type="submit"
+            className="btn btn-default btn-block">Save</button>
         </form>
       </div>
 
