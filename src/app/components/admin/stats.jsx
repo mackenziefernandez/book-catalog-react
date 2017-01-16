1c5128c162ec7f8f1c43174f2546f9bc1e3ea793
mfernandez-rally <mfernandez@rallydev.com>
2017-01-16 16:55:10 -0700

    New book goals

--- src/app/components/admin/stats.jsx
+++ src/app/components/admin/stats.jsx
@@ -45,19 +45,23 @@ class Stats extends Component {
 
 
     const unread = Object.filter(this.props.books, book => book.status === false);
+    const readThisYear = Object.filter(this.props.books, book => new Date(book.dateFinished).getFullYear() === 2017);
     const numUnread = Object.keys(unread).length;
     const numBooks = Object.keys(this.props.books).length;
     const numRead = numBooks - numUnread;
+    const numReadThisYear = Object.keys(readThisYear).length;
     return (
       <div>
         <h2>Book Stats Page!</h2>
         <p>{numBooks} total books</p>
         <p>{numUnread} unread books, {numBooks - numUnread} books completed</p>
         <p>{(((numBooks - numUnread)/numBooks)*100).toFixed(3)}% of books read</p>
-        <h2>Goal: Get to 80% books read</h2>
+        <h2>Goal: Keep 80% of books read</h2>
         <p>{(numBooks*.8 - numRead).toFixed(0)} books to go!</p>
         <h2>Goal: Get to 1000 books read</h2>
         <p>{(1000 - numRead).toFixed(0)} books to go!</p>
+        <h2>Goal: Read 50 books in 2017</h2>
+        <p>{numReadThisYear} books read so far ({(50 - numReadThisYear).toFixed(0)} remaining)</p>
       </div>
     )
   }
