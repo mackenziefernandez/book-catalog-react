493007c39894ef820e729f2403d41f3b66f8495d
mfernandez-rally <mfernandez@rallydev.com>
2017-01-26 09:42:57 -0700

    Remove console logs

--- src/app/reducers/bookmark_reducer.js
+++ src/app/reducers/bookmark_reducer.js
@@ -10,8 +10,6 @@ See `initialstate.js` for a clear view of what it looks like!
 */
 
 export default function (state = null, action) {
-  console.log('state', state);
-  console.log('action', action);
   switch (action.type) {
     case FETCH_BOOKMARKS:
       return action.payload;
